78f41a4ec700ddd6005e60e249a42e80b85fd057
Mehdi BOUDOUR <mehdiboudour.contact@gmail.com>
2021-12-09 16:43:02 +0100

    component.ts selectors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hello-angular'`, () => {
+  it(`should have as title 'Mehdi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-angular');
+    expect(app.name).toEqual('Mehdi');
   });
 
   it('should render title', () => {
